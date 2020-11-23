89e4d9285ea74348bc3bc7fa47d6788f55147b2b
wangzefan666 <214285159@qq.com>
2020-11-23 17:23:54 +0800

    optim code

--- code/utils.py
+++ code/utils.py
@@ -18,18 +18,12 @@ def read_pkl(path):
 
 def predict_on_batch(sess, predict_func, test_x, test_t, test_g, batchsize=800):
     n_samples_test = test_x.shape[0]
-    n_batch_test = n_samples_test // batchsize
     test_pred = np.zeros(n_samples_test)
-    for i_batch in range(n_batch_test):
-        batch_x = test_x.iloc[i_batch * batchsize:(i_batch + 1) * batchsize]
-        batch_t = test_t[i_batch * batchsize:(i_batch + 1) * batchsize]
-        batch_g = test_g[i_batch * batchsize:(i_batch + 1) * batchsize]
+    batch = [(start, min(start + batchsize, n_samples_test)) for start in range(0, n_samples_test, batchsize)]
+    for (start, end) in batch:
+        batch_x = test_x.iloc[start:end]
+        batch_t = test_t[start:end]
+        batch_g = test_g[start:end]
         _pred = predict_func(sess, batch_x, batch_t, batch_g)
-        test_pred[i_batch * batchsize:(i_batch + 1) * batchsize] = _pred.reshape(-1)
-    if n_batch_test * batchsize < n_samples_test:
-        batch_x = test_x.iloc[n_batch_test * batchsize:]
-        batch_t = test_t[n_batch_test * batchsize:]
-        batch_g = test_g[n_batch_test * batchsize:]
-        _pred = predict_func(sess, batch_x, batch_t, batch_g)
-        test_pred[n_batch_test * batchsize:] = _pred.reshape(-1)
+        test_pred[start:end] = _pred.reshape(-1)
     return test_pred
